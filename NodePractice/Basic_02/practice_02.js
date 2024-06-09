8820e1fc003537030472a26190fad60d3a77e2e4
pinal makwana <80570085+pinal2704@users.noreply.github.com>
2024-06-09 06:35:14 +0000

    small change

--- NodePractice/Basic_02/practice_02.js
+++ NodePractice/Basic_02/practice_02.js
@@ -1,18 +1,46 @@
 
 
-const myArray = [32,232,3,2,323,32,3,]
+// const myArray = [32,232,3,2,323,32,3,]
 
-const myArray2 = [26,32,232,3,2,323,32,3]
-
-
-// console.log(myArray.concat(myArray2));
-
-// myArray.push(6)
-myArray2.shift()
-
-console.log(myArray2);
-
-const myn1 = myArray.slice(1,3);
+// const myArray2 = [26,32,232,3,2,323,32,3]
 
 
-console.log(myn1);
+// // console.log(myArray.concat(myArray2));
+
+// // myArray.push(6)
+// myArray2.shift()
+
+// // console.log(myArray2);
+
+// const myn1 = myArray.slice(1,3);
+
+
+// // console.log(myn1);
+
+
+// const Myobject = new Object()
+
+// const object = {
+
+//     name :"pinal",
+//     age : 25,
+//     Location : "Amedabad",
+//     fullname : {
+//             userFullNam :{
+//                 User : "pinal"  
+//             }
+//     }
+
+// }
+// console.log(object.fullname.userFullNam);
+
+
+var a=10;
+let b=20;
+const c=30;
+
+
+
+
+
+
